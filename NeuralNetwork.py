72aa682b9110c3c550c75cd71bdde353ff0ee11e
Dimitry Foures <dimitry.foures@gmail.com>
2014-09-19 17:01:39 +0100

    parallel NN + notebook

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,38 +3,33 @@ import random as rand
 from ffnet import ffnet, mlgraph
 import networkx as NX
 from joblib import Parallel, delayed
-from copy import copy, deepcopy
+from copy import copy
 
-
-# This functions are outside the class to make the parallel implementation possible
-def cross_validation( net, input, output, ratio, ntry, iteration, nproc = 1):
-    (test_input, train_input, test_output, train_output) = net.random_split(input, output, ratio)
-    net.train_best(train_input = train_input, train_output = train_output, ntry = ntry, in_cv = True, test_input = test_input, test_output = test_output, nproc = nproc)
-    cv_weights = net.weights
-    train_error = net.sqerror(train_input, train_output)
-    test_error = net.sqerror(test_input, test_output)
-    if net.verbose:
-        print 'Cross-validation {0}: train-error {1:.2e}, test-error {2:.2e}\n'.format(iteration,train_error,test_error),
-    return net
-
-def train(net , train_input, train_output, iteration, in_cv, test_input = None, test_output = None):
+def train_once(net, train_input, train_output, in_cv, test_input, test_output, i):
     net.randomweights()
     net.train_cg(train_input, train_output, disp = 0)
-    weights = net.weights
     train_error = net.sqerror(train_input,train_output)
     if in_cv:
         test_error = net.sqerror(test_input,test_output)
     else:
         test_error = None
-    if net.verbose == 2:
+    if net.verbose:
         if in_cv:
-            print 'Optimisation {0}: train-error {1:.2e}, test-error {2:.2e}'.format(iteration,train_error,test_error)
+            print 'Optimisation {0}: train-error {1:.2e}, test-error {2:.2e}'.format(i,train_error,test_error)
         else:
-            print 'Optimisation {0}: train-error {1:.2e}'.format(iteration,train_error)
-    return net, train_error, test_error
+            print 'Optimisation {0}: train-error {1:.2e}'.format(i,train_error)
+    return train_error, test_error, net
 
+def cross_validation(net, input, output, ratio, ntry, i, nproc):
+    (test_input, train_input, test_output, train_output) = net.random_split(input, output, ratio)
+    net.train_best(train_input = train_input, train_output = train_output, ntry = ntry, in_cv = True, test_input = test_input, test_output = test_output, nproc = nproc)
+    train_error = net.sqerror(train_input, train_output)
+    test_error = net.sqerror(test_input, test_output)
+    if net.verbose:
+        print 'Cross-validation {0}: train-error {1:.2e}, test-error {2:.2e}'.format(i,train_error,test_error)
+    return net
 
-class NeuralNetwork(object):
+class NeuralNetwork(ffnet):
 
     def __init__(self, arch, verbose = False):
         """
@@ -43,7 +38,7 @@ class NeuralNetwork(object):
         self.verbose = verbose
         self.arch = arch
         conec = mlgraph(self.arch, biases=True)
-        self._net = ffnet(conec)
+        ffnet.__init__(self, conec)
 
     def train_cv(self, input, output, ratio = 0.75, ncv = 10, ntry = 10, nproc = 1):
         """
@@ -56,18 +51,13 @@ class NeuralNetwork(object):
             print 'r=1. Use train_best instead...'
             self.train_best(input, output, ntry)
         else:
-            nsample = np.shape(input)[0]
-            weights = []
-            train_error = []
-            test_error = []
-            net = deepcopy(self)
-            self.cv_nets = Parallel(n_jobs = nproc, verbose = 0)(
-                delayed(cross_validation)(net, input, output, ratio = ratio, ntry = ntry, iteration = i, nproc = nproc)
-                    for i in range(1,ncv+1))
+            self.cv_nets =   Parallel(n_jobs = nproc)(
+                                delayed(cross_validation)(self, input, output, ratio, ntry, i, nproc)
+                                    for i in range(1,ntry+1))
 
     def random_split(self, input, output, ratio = 0.75):
         """
-        Splits arrays in two datasets train and test according to the ratio specified
+        Splits input and output arrays in two datasets each (test and train) according to the ratio specified
         """
         nsample = np.shape(input)[0]
         assert nsample == np.shape(output)[0]
@@ -86,44 +76,36 @@ class NeuralNetwork(object):
 
     def train_best(self, train_input, train_output, ntry = 10, in_cv = False, test_input = None, test_output = None, nproc = 1):
         """
-        Finds best neural network amongst ntry trained neural networks
+        Finds best neural network amongst 10 trained neural networks
         The training method is a conjugate gradient descent
         The fitness is measured by sqerror
         """
+        results =   Parallel(n_jobs = nproc)(
+                        delayed(train_once)(self, train_input, train_output, in_cv, test_input, test_output, i)
+                            for i in range(1,ntry+1))
+        train_error = [result[0] for result in results]
+        test_error = [result[1] for result in results]
+        net = [result[2] for result in results]
+        self.best_net = net[np.argmin(train_error)]
+        self.__dict__.update(self.best_net.__dict__)
 
-        # Because the parallel loop copies the object self, we have to add this extra line.
-        train_input, train_output = self._setnorm(input = train_input, target = train_output)
-
-        results = Parallel(n_jobs = nproc, verbose = 0)(
-                delayed(train)(self, train_input, train_output, iteration = i, in_cv = in_cv, test_input = test_input, test_output = test_output)
-                    for i in range(1,ntry+1))
-
-        nets = [result[0] for result in results]
-        train_error = [result[1] for result in results]
-        test_error = [result[2] for result in results]
-
-        if self.verbose == 2:
-            if in_cv:
-                print 'Optimisation finished: train-error {0:.2e}, test-error {1:.2e}'.format(np.min(train_error),np.min(test_error))
-            else:
-                print 'Optimisation finished: train-error {0:.2e}'.format(np.min(train_error))
-        self.best_net = nets[np.argmin(train_error)]
-
-
-    def sim(self, input, cv = False, best = False, net = None):
+    def sim(self, input, cv = False, best = False, cv_i = False):
         """
         Simulates the result of the neural network obtained by either training method
         """
         if cv:
             output_list = []
             for i in range(self.ncv):
-                output_list += [self.cv_nets[i].call(input)]
+                self.__dict__.update(self.cv_nets[i].__dict__)
+                output_list += [self.call(input)]
             output = np.concatenate(output_list, axis = 1)
             mean_output = np.mean(output,1)
             nsample = np.shape(input)[0]
             return np.resize(mean_output,(nsample,self.arch[-1]))
         if best:
-            return self.best_net.call(input)
-        if net is not None:
-            return net.call(input)
+            self.__dict__.update(self.best_net.__dict__)
+            return self.call(input)
+        if cv_i:
+            self.__dict__.update(self.cv_nets[cv_i].__dict__)
+            return self.call(input)
         return self.call(input)
